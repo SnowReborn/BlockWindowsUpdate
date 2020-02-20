6436a7ad6a934cfc907ec241da877eeefe6a5a54
SnowReborn <36327107+SnowReborn@users.noreply.github.com>
2020-02-19 23:37:27 -0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ShowInterfaceToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TimToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Fuck WIndows Update"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowInterfaceToolStripMenuItem, Me.TimToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(202, 68)
+        '
+        'ShowInterfaceToolStripMenuItem
+        '
+        Me.ShowInterfaceToolStripMenuItem.Name = "ShowInterfaceToolStripMenuItem"
+        Me.ShowInterfaceToolStripMenuItem.Size = New System.Drawing.Size(201, 32)
+        Me.ShowInterfaceToolStripMenuItem.Text = "Show Interface"
+        '
+        'TimToolStripMenuItem
+        '
+        Me.TimToolStripMenuItem.Name = "TimToolStripMenuItem"
+        Me.TimToolStripMenuItem.Size = New System.Drawing.Size(201, 32)
+        Me.TimToolStripMenuItem.Text = "Quit"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 5000
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(287, 215)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(204, 92)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start on Startup"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(287, 324)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(204, 92)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Unregister from Startup"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(59, 203)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(138, 63)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Disable Update"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(59, 283)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(138, 66)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Unblock Update"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(60, 127)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(325, 32)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Windows Update Status:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(376, 129)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 32)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "??"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(245, 59)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 32)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "??"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(59, 367)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(138, 65)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "Turn on Update"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(531, 460)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Winwodws Update Disabler"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents TimToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ShowInterfaceToolStripMenuItem As ToolStripMenuItem
+    Protected WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.ServiceProcess
+Public Class Form1
+
+    'Public Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As System.Windows.Forms.Keys) As Short //for hotkey
+
+    Private Sub TimToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TimToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            Dim WindowsUpdateService As New System.ServiceProcess.ServiceController("wuauserv")
+            Dim ST As String = WindowsUpdateService.StartType
+            Label2.Text = "Disabled"
+            Label2.ForeColor = Color.ForestGreen
+            Label3.Text = WindowsUpdateService.Status
+            If ST IsNot "4" Or WindowsUpdateService.Status = System.ServiceProcess.ServiceControllerStatus.Running Then
+                Shell("sc stop wuauserv", AppWinStyle.Hide)
+                Shell("sc config wuauserv start= disabled", AppWinStyle.Hide)
+            End If
+        Catch ex As Exception
+            MsgBox("Attempt to disable Update Failed", MsgBoxStyle.Critical, "Error")
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim aPath As String = Application.ExecutablePath
+            Dim RegKey As String = "powershell $Sta = New-ScheduledTaskAction -Execute " + "'" + aPath + "'" + " ; $Stt = New-ScheduledTaskTrigger -AtLogOn ; $STPrin = New-ScheduledTaskPrincipal -GroupId " + """" + "BUILTIN\Administrators" + """" + " -RunLevel Highest ;  $Stset = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 2 -AllowStartIfOnBatteries -DisallowHardTerminate ;Register-ScheduledTask WinUpdateDisabler -Action $Sta -Settings $Stset -Principal $STPrin -Trigger $Stt"
+            Shell(RegKey, AppWinStyle.Hide)
+        Catch ex As Exception
+            MsgBox("Error Creating Tasks", MsgBoxStyle.Critical, "Error")
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim aPath As String = Application.ExecutablePath
+            Dim UnRegKey As String = "powershell Unregister-ScheduledTask -TaskName " + """" + "WinUpdateDisabler" + """" + " -Confirm:$False"
+            Shell(UnRegKey, AppWinStyle.Hide)
+        Catch ex As Exception
+            MsgBox("Error Creating Tasks", MsgBoxStyle.Critical, "Error")
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Timer1.Enabled = False
+        Label2.Text = "Unblocked"
+        Label2.ForeColor = Color.Orange
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Show()
+            Me.WindowState = FormWindowState.Normal
+            Me.BringToFront()
+        Else
+            Me.Hide()
+            Me.WindowState = FormWindowState.Minimized
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hide()
+        NotifyIcon1.Visible = False
+        NotifyIcon1.Visible = True
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Timer1.Enabled = False
+        Label2.Text = "Running"
+        Label2.ForeColor = Color.Red
+        Shell("sc config wuauserv start=demand", AppWinStyle.Hide)
+        Shell("sc start wuauserv", AppWinStyle.Hide)
+    End Sub
+
+    Private Sub ShowInterfaceToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShowInterfaceToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+
+End Class
